c05b8852823b81e7a75fffd28a3cb05bd05e7334
Eternal128 <jamwilhan@gmail.com>
2026-03-05 13:50:47 -0500

    button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import {Box} from "lucide-react";
 
 const Navbar = () => {
+    const handleAuthClick = async () => {};
+
     return (
         <div>
-            Navbar
+            <header className="navbar">
+                <nav className="inner">
+                    <div className="left">
+                        <div className="brand">
+                            <Box className="logo" />
+                            <span className="name">Roomie</span>
+                        </div>
+
+                            <ul className={"links"}>
+                                <a href="#">Product</a>
+                                <a href="#">Pricing</a>
+                                <a href="#">Community</a>
+                                <a href="#">Enterprise</a>
+                            </ul>
+                    </div>
+
+                    <div className={"actions"}>
+                        <button onClick={handleAuthClick} className={"login"}>
+                            Log In
+                        </button>
+
+                        <a href="#upload" className={"cta"}> Get Started </a>
+                    </div>
+                </nav>
+            </header>
         </div>
     )
 }
